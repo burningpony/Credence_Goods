8535b5783e6bfa4815c6f73271f00dd8c6e251f2
Russell Osborne <russell.osborne@framebridge.com>
2018-12-06 22:02:24 -0500

    Removes unused function

--- app/javascript/components/helpers/function.js
+++ app/javascript/components/helpers/function.js
@@ -1,6 +1,11 @@
 
 const invphi = (Math.sqrt(5) - 1) / 2; // 1/phi
 const invphi2 = (3 - Math.sqrt(5)) / 2; // 1/phi^2
+
+// This function is converted from python example of the
+// Golden Section search article on wikipedia
+// https://en.wikipedia.org/wiki/Golden-section_search
+//
 // gr = (math.sqrt(5) + 1) / 2
 
 const gr = (Math.sqrt(5) + 1) / 2; // 1/phi
@@ -47,41 +52,6 @@ const gss = (f, a, b, tol = 1e-5) => {
   }
 
   return (b + a) / 2;
-};
-
-// Not Used
-const gssrec = (f, a, b, tol, {
-  h, c, d, fc, fd,
-}) => {
-  // Golden section search, recursive.
-
-  // Given a function f with a single local minimum in
-  // the interval [a,b], gss returns a subset interval
-  // [c,d] that contains the minimum with d-c <= tol.
-
-  // example:
-  // const f = x => (x - 2) ** 2;
-  // const a = 1;
-  // const b = 5;
-  // const tol = 1e-5;
-  // console.log(gssrec(f, a, b, tol, {}));
-  // (1.9999959837979107, 2.0000050911830893)
-  [a, b] = [Math.min(a, b), Math.max(a, b)];
-  if (h === undefined) { h = b - a; }
-  if (h <= tol) { return [a, b]; }
-  if (c === undefined) { c = a + invphi2 * h; }
-  if (d === undefined) { d = a + invphi * h; }
-  if (fc === undefined) { fc = f(c); }
-  if (fd === undefined) { fd = f(d); }
-  if (fc < fd) {
-    return gssrec(f, a, d, tol, {
-      h: h * invphi, c: undefined, fc: undefined, d: c, fd: fc,
-    });
-  }
-
-  return gssrec(f, c, b, tol, {
-    h: h * invphi, c: d, fc: fd, d: undefined, fd: undefined,
-  });
 };
 
 
