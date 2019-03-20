9ec699ecffc51d34c0427c83540ba9df91dc3411
Emmanuel Velez <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2019-03-20 16:37:02 -0500

    updating url to wss

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -5,7 +5,7 @@ import ActionCable from 'actioncable';
 import store from './stores/index';
 import Experiment from './experiment';
 import 'bootstrap/dist/css/bootstrap.min.css';
-export const API_WS_ROOT = 'ws://credence-goods.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://credence-goods.herokuapp.com/cable';
 export const cable = ActionCable.createConsumer(API_WS_ROOT);
 
 document.addEventListener('DOMContentLoaded', () => {
