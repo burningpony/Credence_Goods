c5fbeba2ab1f269591845d59d67d65d6ae01ac5b
Emmanuel Velez <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2019-03-20 16:09:50 -0500

    db schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_173920) do
+ActiveRecord::Schema.define(version: 2019_03_20_210636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_173920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "group_id"
+    t.string "role"
     t.index ["group_id"], name: "index_users_on_group_id"
   end
 
