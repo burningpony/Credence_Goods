9867eb9c2d1411972c13d41a97640a6c0dba795f
Emmanuel Velez <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2019-03-20 22:38:37 -0500

    Enabling http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  # http_basic_authenticate_with name: "sam", password: "sam", except: :index
+  http_basic_authenticate_with name: 'user', password: '123456789'
 end
