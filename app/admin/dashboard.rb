12842a01740e33a2f2ae67cac150c247d175bf9f
scong <stcongleton@gmail.com>
2019-09-09 17:26:47 -0400

    Fix desc order by.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    table_for User.where("finished_at is not null").order(finished_at: desc) do
+    table_for User.where("finished_at is not null").order(finished_at: :desc) do
       column "Id",     :id
       column "Total Payment",   :total_payment
       column "Part A Payment", :part_a_payment
