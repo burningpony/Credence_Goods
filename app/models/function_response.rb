1ad91912fddf36fe7386d46a7c242a3e6559f093
scong <stcongleton@gmail.com>
2019-09-09 02:14:34 -0400

    Comment out line that seems to crash everything.

--- app/models/function_response.rb
+++ app/models/function_response.rb
@@ -1,6 +1,6 @@
 class FunctionResponse < ApplicationRecord
     belongs_to :function
-    scope :find_by_user_id, -> (user_id) { where(:part => 2 ).where(:user_id => user_id) }
+    # scope :find_by_user_id, -> (user_id) { where(:part => 2 ).where(:user_id => user_id) }
     enum part: [ :unknown, :part_1, :part_2]
 
     attr_accessor :set_id
